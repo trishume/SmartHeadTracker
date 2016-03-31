3467454463250502f425fd44f107b396d0eaa921
Tristan Hume <tris.hume@gmail.com>
2016-03-31 11:11:27 -0400

    More robust to changing angle

--- src/markertracking.cpp
+++ src/markertracking.cpp
@@ -13,17 +13,22 @@ typedef cv::Point3_<uint8_t> UPixel;
 typedef cv::Point3_<float> FPixel;
 using namespace cv;
 
+float vecDist(FPixel a, FPixel b) {
+  FPixel diff = a - b;
+  return diff.x*diff.x + diff.y*diff.y + diff.z*diff.z;
+}
+
 void trackMarkers(Mat &m) {
   FPixel target(0.35,0.4,1.0);
+  FPixel target2(0.43,0.55,1.0);
   for (UPixel &p : cv::Mat_<UPixel>(m)) {
     float maxc = std::max(p.x, std::max(p.y, p.z));
     FPixel normalized(p.x / maxc, p.y / maxc, p.z / maxc);
-    FPixel diff = normalized - target;
-    float dist = std::abs(diff.x) + std::abs(diff.y) + std::abs(diff.z);
+    float dist = std::min(vecDist(normalized,target),vecDist(normalized,target2));
     p.x = 0;
-    p.y = (uint8_t)std::min(dist*200.0f, 255.0f);
+    p.y = (uint8_t)std::min(dist*3000.0f, 255.0f);
 
-    p.z = (p.y < 30 && maxc > 150) ? 255 : 0;
+    p.z = (p.y < 25 && maxc > 180) ? 255 : 0;
   }
 
   imshow("main", m);
